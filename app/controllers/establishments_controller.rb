859fa38939b809fe4684d2692d6d2c99300dd72d
Hannah Carnes <hecarnes@gmail.com>
2019-02-18 23:37:45 -0500

    add search

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -1,7 +1,10 @@
 class EstablishmentsController < ApplicationController
   def near_me
     establishments = Establishment.order(["establishments.location <-> ?::geography", "SRID=4326;POINT(#{params[:lng].to_f} #{params[:lat].to_f})"]).limit(50)
-
-    render json: establishments
+    if params[:search]
+      render json: establishments.where("dba ILIKE ?", "%#{params[:search]}%")
+    else
+      render json: establishments
+    end
   end
 end
